0dff5babf34036c2fe4867a326ba7d1e94b6d2ff
Cae <eduardokae01@hotmail.com>
2022-02-15 17:03:05 -0300

    add: myContext com o Provider

---
+++ src/Context/Provider.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import myContext from './myContext';
+
+function Provider({ children }) {
+  const [token, setToken] = useState(null);
+
+  const providerValue = {
+    token,
+    setToken
+  };
+
+  return (
+    <myContext.Provider value={ providerValue }>
+      { children }
+    </myContext.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Provider;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Provider from './Context/Provider';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
